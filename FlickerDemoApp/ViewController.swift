9e57088bb79a6e569ede018ccf8fb7c78beb38d8
charleshs <charlous167@gmail.com>
2020-03-31 14:38:11 +0800

    Initial commit: created Xcode project and added .gitignore.

---
+++ FlickerDemoApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FlickerDemoApp
+//
+//  Created by Charles Hsieh on 2020/3/31.
+//  Copyright © 2020 Charles Hsieh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
